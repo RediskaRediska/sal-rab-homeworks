895983d56930dcfe853e758921adae424cd09f61
RediskaRediska <mr.rediska@ya.ru>
2022-04-10 23:30:38 +0300

    final task

--- task_final/solution.js
+++ task_final/solution.js
@@ -38,18 +38,16 @@ function sendRequest(name, phone, address, goods, sum) {
 
     let countOfGoods = goods.length;
 
-    let goods = {title: [], count: [] }
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
+        for (let i = 0; i < countOfGoods; i += 1) {
         data.goods.push(goods[i].title);
     }
 
-    data.order.address = address;
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}.`;
     data.order.sum = sum;
     
-    data.client = [name + phone];
-
-    let jsonData = JSON.stringify(data);
+    data.client = `${name} ${phone}`;
+    
+    let jsonData = JSON.stringify({data: data});
 
     return jsonData;
 }
